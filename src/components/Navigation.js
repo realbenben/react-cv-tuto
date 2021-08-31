6e5ea7f7d68ff1439f2dc9a673699da25f329422
realbenben <b.patou59@gmail.com>
2021-08-31 14:24:06 +0200

    navbar structure done

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -41,6 +41,49 @@ const Navigation = () => {
           </li>
         </ul>
       </div>
+      <div className="socialNetwork">
+        <ul>
+          <li>
+            <a
+              href="https://benoitpatou.dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://benoitpatou.dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://benoitpatou.dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://benoitpatou.dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-codepen"></i>
+            </a>
+          </li>
+        </ul>
+        <div className="signature">
+          <p>From Spirit since 1985</p>
+        </div>
+      </div>
     </div>
   );
 };
